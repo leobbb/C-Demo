694774ce274207ccbc9bf9e5a89d4d7da56ec743
leobbb <askf@qq.com>
2014-07-23 09:53:06 +0800

    add job5 - test namespace

---
+++ job5/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using City.MySchool.Class;
+
+namespace job5
+{
+    class Student
+    {
+        public string GetStudentName()
+        {
+            City.MySchool.Class.Student stu = new City.MySchool.Class.Student();
+            return stu.name;
+        }
+    }
+}
---
+++ job5/stu1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using user=System.Threading.Tasks;
+
+namespace City
+{
+    namespace MySchool
+    {
+        namespace Class
+        {
+            public class Student{
+                public string name = "";
+            }
+        }
+    }
+    class stu1
+    {
+    }
+}

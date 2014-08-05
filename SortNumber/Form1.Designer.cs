084b48696aa62467e976184016794f392aa3794d
leobbb <askf@qq.com>
2014-08-05 22:24:13 +0800

    数字排序应用测试

---
+++ SortNumber/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace SortNumber
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSort = new System.Windows.Forms.Button();
+            this.lblShow = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "依次输入数字";
+            // 
+            // txtNum
+            // 
+            this.txtNum.Location = new System.Drawing.Point(126, 23);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(100, 20);
+            this.txtNum.TabIndex = 1;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(260, 21);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "添加";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnSort
+            // 
+            this.btnSort.Location = new System.Drawing.Point(260, 59);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(75, 23);
+            this.btnSort.TabIndex = 0;
+            this.btnSort.Text = "排序";
+            this.btnSort.UseVisualStyleBackColor = true;
+            this.btnSort.Click += new System.EventHandler(this.btnSort_Click);
+            // 
+            // lblShow
+            // 
+            this.lblShow.AutoSize = true;
+            this.lblShow.Location = new System.Drawing.Point(64, 100);
+            this.lblShow.Name = "lblShow";
+            this.lblShow.Size = new System.Drawing.Size(0, 13);
+            this.lblShow.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(475, 258);
+            this.Controls.Add(this.lblShow);
+            this.Controls.Add(this.btnSort);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtNum);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "排序";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNum;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSort;
+        private System.Windows.Forms.Label lblShow;
+    }
+}
+
---
+++ SortNumber/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SortNumber
+{
+    public partial class Form1 : Form
+    {
+        //输入一个数字序列，对其进行排序
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        StringBuilder str = new StringBuilder();
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            str.Append(txtNum.Text + ",");
+            lblShow.Text = "输入的序列为： " + str.ToString();
+            txtNum.Text = "";
+        }
+
+        private void btnSort_Click(object sender, EventArgs e)
+        {
+            // 把字符串以“，”分隔为字符串数组，并且去掉空字符串           
+            string[] numStr = str.ToString().Split(",".ToCharArray(), StringSplitOptions.RemoveEmptyEntries); 
+
+            lblShow.Text += "\n共输入 " + numStr.Length + " 个数";
+
+            float[] numArr = new float[numStr.Length];
+
+            //把字符串数组转化为浮点数数组
+            for (int i = 0; i < numStr.Length; i++)
+            {
+                numArr[i] = float.Parse(numStr[i]);
+            }
+
+            Array.Sort(numArr);
+            lblShow.Text += "\n排序后序列为：";
+            for (int j = 0; j < numArr.Length; ++j)
+            {
+                lblShow.Text += numArr[j] + "   ";
+            }
+          
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnAdd.Focus();
+        }
+    }
+}

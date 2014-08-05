2d56ee3edf0cd11cbc6dc6f36398abc609c70272
leobbb <askf@qq.com>
2014-08-05 15:51:35 +0800

    test Enum type

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace WinForm1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblShow = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblShow
+            // 
+            this.lblShow.AutoSize = true;
+            this.lblShow.Location = new System.Drawing.Point(67, 84);
+            this.lblShow.Name = "lblShow";
+            this.lblShow.Size = new System.Drawing.Size(35, 13);
+            this.lblShow.TabIndex = 0;
+            this.lblShow.Text = "label1";
+            this.lblShow.Move += new System.EventHandler(this.lblShow_Move);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblShow);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Test_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblShow;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForm1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        enum MyEnum { a = 101, b, c, d = 201, e, f };
+        private void Test_Load(object sender, EventArgs e)
+        {
+            MyEnum x = MyEnum.f;
+            MyEnum y = (MyEnum)202;
+            string result = "枚举数x的值为" + (int)x;
+            result += "\n枚举数y代表枚举元素" + y;
+            lblShow.Text = result;
+        }
+
+        private void lblShow_Move(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

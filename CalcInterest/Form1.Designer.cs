6086dc2680043d513efc8540abd1598b19fdce3c
leobbb <askf@qq.com>
2014-08-05 20:12:49 +0800

    利息计算器

---
+++ CalcInterest/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace CalcInterest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMoney = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtRate = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lstYear = new System.Windows.Forms.ListBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lblShow = new System.Windows.Forms.Label();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "本金";
+            // 
+            // txtMoney
+            // 
+            this.txtMoney.Location = new System.Drawing.Point(93, 38);
+            this.txtMoney.Name = "txtMoney";
+            this.txtMoney.Size = new System.Drawing.Size(100, 20);
+            this.txtMoney.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "利率";
+            // 
+            // txtRate
+            // 
+            this.txtRate.Location = new System.Drawing.Point(93, 67);
+            this.txtRate.Name = "txtRate";
+            this.txtRate.Size = new System.Drawing.Size(100, 20);
+            this.txtRate.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(43, 106);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "存期";
+            // 
+            // lstYear
+            // 
+            this.lstYear.FormattingEnabled = true;
+            this.lstYear.Items.AddRange(new object[] {
+            "1年",
+            "2年",
+            "3年",
+            "4年"});
+            this.lstYear.Location = new System.Drawing.Point(93, 102);
+            this.lstYear.Name = "lstYear";
+            this.lstYear.Size = new System.Drawing.Size(65, 43);
+            this.lstYear.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(199, 41);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(19, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "元";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(199, 74);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(15, 13);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "%";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(43, 161);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(43, 13);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "本息和";
+            // 
+            // lblShow
+            // 
+            this.lblShow.Location = new System.Drawing.Point(93, 161);
+            this.lblShow.Name = "lblShow";
+            this.lblShow.Size = new System.Drawing.Size(125, 46);
+            this.lblShow.TabIndex = 4;
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(197, 101);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(75, 23);
+            this.btnCalc.TabIndex = 4;
+            this.btnCalc.Text = "计算";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(197, 210);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "清空";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.lblShow);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lstYear);
+            this.Controls.Add(this.txtRate);
+            this.Controls.Add(this.txtMoney);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "利息计算器";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMoney;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtRate;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox lstYear;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lblShow;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ CalcInterest/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalcInterest
+{
+    public partial class Form1 : Form
+    {
+        private double _money;
+
+        public double Money
+        {
+            get { return _money; }
+            set { _money = value; }
+        }
+        private double _rate;
+
+        public double Rate
+        {
+            get { return _rate; }
+            set { _rate = value; }
+        }
+        private int _time;
+
+        public int Time
+        {
+            get { return _time; }
+            set { _time = value; }
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtMoney.Text = "10000";
+            txtRate.Text = "2.25";
+            lstYear.SelectedItem = "2年";
+            btnCalc.Focus();
+            
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            double sum = CalcInt();
+            sumShow(sum);
+        }
+
+        private void sumShow(double sum)
+        {
+            lblShow.Text = "本金为 " + txtMoney.Text + " 元\n" + "存期为 " + lstYear.SelectedItem + " \n";
+            lblShow.Text += "本息和为 " + sum + " 元";
+
+        }
+
+        private double CalcInt()
+        {
+            double sum = 0;
+            int time = 0;
+            string str = (string)lstYear.SelectedItem;
+            switch (str)
+            {
+                case "1年" :
+                    time = 1;
+                    break;
+                case "2年" :
+                    time = 2;
+                    break;
+                case "3年":
+                    time = 3;
+                    break;
+                case "4年" :
+                    time = 4;
+                    break;
+
+            }
+            sum = double.Parse(txtMoney.Text) * double.Parse(txtRate.Text) / 100 * time;
+            sum += double.Parse(txtMoney.Text);
+            return sum;
+        }
+    }
+}

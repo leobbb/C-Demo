68af04782294db77e7faf04a2dbdfd5e21d949a5
leobbb <askf@qq.com>
2014-08-05 22:52:57 +0800

    字符统计应用测试

---
+++ CounterLowerChar/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace CounterLowerChar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtContent = new System.Windows.Forms.TextBox();
+            this.btnSea = new System.Windows.Forms.Button();
+            this.lblShow = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "请输入一行英文字符：";
+            // 
+            // txtContent
+            // 
+            this.txtContent.Location = new System.Drawing.Point(21, 78);
+            this.txtContent.Name = "txtContent";
+            this.txtContent.Size = new System.Drawing.Size(149, 20);
+            this.txtContent.TabIndex = 1;
+            // 
+            // btnSea
+            // 
+            this.btnSea.Location = new System.Drawing.Point(186, 76);
+            this.btnSea.Name = "btnSea";
+            this.btnSea.Size = new System.Drawing.Size(75, 23);
+            this.btnSea.TabIndex = 2;
+            this.btnSea.Text = "查询";
+            this.btnSea.UseVisualStyleBackColor = true;
+            this.btnSea.Click += new System.EventHandler(this.btnSea_Click);
+            // 
+            // lblShow
+            // 
+            this.lblShow.AutoSize = true;
+            this.lblShow.Location = new System.Drawing.Point(21, 135);
+            this.lblShow.Name = "lblShow";
+            this.lblShow.Size = new System.Drawing.Size(0, 13);
+            this.lblShow.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblShow);
+            this.Controls.Add(this.btnSea);
+            this.Controls.Add(this.txtContent);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "统计字符数量";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtContent;
+        private System.Windows.Forms.Button btnSea;
+        private System.Windows.Forms.Label lblShow;
+    }
+}
+
---
+++ CounterLowerChar/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CounterLowerChar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSea_Click(object sender, EventArgs e)
+        {
+            //统计英文字母的个数
+            int n = 0, i = 0;
+            int m = txtContent.Text.Length;
+            do
+            {
+                char c = txtContent.Text[i++];
+                if (c > 'A' && c <= 'Z' || c >= 'a' && c <= 'z')
+                    n++;
+            } while (i != m);
+
+            lblShow.Text = String.Format("共输入{1} 个字符，其中英文字母有： {0} 个。", n, m);
+        }
+    }
+}

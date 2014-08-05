43df72c32320dae68d8c98f66c612cdb93d8417e
leobbb <askf@qq.com>
2014-08-05 16:30:16 +0800

    隐式类型转换测试

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -28,15 +28,23 @@ namespace WinForm1
             //result += "\n枚举数y代表枚举元素" + y;
             //lblShow.Text = result;
 
-            //算数运算符的应用测试
-            int i = 1, j = 1, p, q;
-            p = (i++) + (i++) + (i++);
-            q = (++j) + (++j) + (++j);
-            lblShow.Text = "变量i的值为" + i;
-            lblShow.Text += "\n变量j的值为" + j;
-            lblShow.Text += "\n变量p的值为" + p;
-            lblShow.Text += "\n变量q的值为" + q;
+            ////算数运算符的应用测试
+            //int i = 1, j = 1, p, q;
+            //p = (i++) + (i++) + (i++);
+            //q = (++j) + (++j) + (++j);
+            //lblShow.Text = "变量i的值为" + i;
+            //lblShow.Text += "\n变量j的值为" + j;
+            //lblShow.Text += "\n变量p的值为" + p;
+            //lblShow.Text += "\n变量q的值为" + q;
 
+            // 赋值运算符及隐式数据类型转换测试
+            int a, b = 5;
+            char c = '啊';
+            a = c;
+            float x = 42;
+            x /= b;
+            lblShow.Text = "整形变量a的值为" + a;
+            lblShow.Text += "\n浮点型变量x的值为" + x;
         }
 
         private void lblShow_Move(object sender, EventArgs e)

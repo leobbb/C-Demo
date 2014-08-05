1a60762653636312d23ab0c6db8469dd5d8f8961
leobbb <askf@qq.com>
2014-08-05 16:23:22 +0800

    算术运算符测试

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -18,13 +18,25 @@ namespace WinForm1
         }
 
         enum MyEnum { a = 101, b, c, d = 201, e, f };
+
         private void Test_Load(object sender, EventArgs e)
         {
-            MyEnum x = MyEnum.f;
-            MyEnum y = (MyEnum)202;
-            string result = "枚举数x的值为" + (int)x;
-            result += "\n枚举数y代表枚举元素" + y;
-            lblShow.Text = result;
+            //枚举类型测试
+            //MyEnum x = MyEnum.f;
+            //MyEnum y = (MyEnum)202;
+            //string result = "枚举数x的值为" + (int)x;
+            //result += "\n枚举数y代表枚举元素" + y;
+            //lblShow.Text = result;
+
+            //算数运算符的应用测试
+            int i = 1, j = 1, p, q;
+            p = (i++) + (i++) + (i++);
+            q = (++j) + (++j) + (++j);
+            lblShow.Text = "变量i的值为" + i;
+            lblShow.Text += "\n变量j的值为" + j;
+            lblShow.Text += "\n变量p的值为" + p;
+            lblShow.Text += "\n变量q的值为" + q;
+
         }
 
         private void lblShow_Move(object sender, EventArgs e)

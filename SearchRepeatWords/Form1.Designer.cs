1b3ae641d576779da9f98fcd22bf3f17d082d60c
leobbb <askf@qq.com>
2014-08-08 22:53:34 +0800

    查询重复出现的双字词汇应用

---
+++ SearchRepeatWords/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace SearchRepeatWords
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtContent = new System.Windows.Forms.TextBox();
+            this.lblShow = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(52, 98);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "查询";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "请输入一行文字：";
+            // 
+            // txtContent
+            // 
+            this.txtContent.Location = new System.Drawing.Point(52, 56);
+            this.txtContent.Name = "txtContent";
+            this.txtContent.Size = new System.Drawing.Size(481, 20);
+            this.txtContent.TabIndex = 1;
+            // 
+            // lblShow
+            // 
+            this.lblShow.AutoSize = true;
+            this.lblShow.Location = new System.Drawing.Point(49, 163);
+            this.lblShow.Name = "lblShow";
+            this.lblShow.Size = new System.Drawing.Size(35, 13);
+            this.lblShow.TabIndex = 3;
+            this.lblShow.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(611, 288);
+            this.Controls.Add(this.lblShow);
+            this.Controls.Add(this.txtContent);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSearch);
+            this.Name = "Form1";
+            this.Text = "查询重复的双字词汇";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtContent;
+        private System.Windows.Forms.Label lblShow;
+    }
+}
+
---
+++ SearchRepeatWords/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SearchRepeatWords
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        struct RepeatWords
+        {
+            public string words;
+            public int num;
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string str = txtContent.Text.Trim();
+            RepeatWords[] rst = new RepeatWords[10];
+            int sum = 0;
+            SearchRW(ref rst, out sum, str);
+
+            if (rst[0].words == null)
+            {
+                lblShow.Text = "输入的文字中没有重复的双字词汇。";
+            }
+            else
+            {
+                ShowRst(rst,sum);
+            }
+            
+        }
+
+        private void ShowRst(RepeatWords[] rst, int sum)
+        {
+            lblShow.Text = String.Format("一共有 {0} 个重复词汇。\n其中：", sum);
+            for (int i = 0; i < sum; ++i)
+            {
+                lblShow.Text += String.Format("\"{0}\"重复了 {1} 次，", rst[i].words, rst[i].num);
+            }
+            lblShow.Text += "。";
+        }
+        private void SearchRW(ref RepeatWords[] rst,out int sum, string str)
+        {
+            sum = 0;
+            for (int j = 0, i = 0; i < str.Length - 3; ++i)
+            {
+                if (ExistInResult(ref rst, str[i].ToString() + str[i + 1].ToString()))
+                {
+                    continue;
+                }
+
+                for (j = i + 2; j < str.Length -1; ++j)
+                {
+                    if (str[i] == str[j] && str[i+1] == str[j+1] )
+                    {
+                        rst[sum].num += 1;
+                        rst[sum].words = str[i].ToString() + str[i + 1].ToString();
+                    }
+                }
+                if (rst[sum].num != 0)
+                {
+                    ++sum;
+                }
+
+            }
+        }
+
+        private bool ExistInResult(ref RepeatWords[] rst, string p)
+        {
+            for (int i = 0; i < rst.Length; ++i)
+            {
+                if (rst[i].num == 0)
+                {
+                    break;
+                }
+                if (rst[i].words == p)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+    }
+}

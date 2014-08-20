339c83404e7ea4b3f31009f0922a6a0b2bf8e42b
leobbb <askf@qq.com>
2014-08-20 14:37:33 +0800

    测试委托类型

---
+++ Test8_1/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Test8_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNuma = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNumb = new System.Windows.Forms.TextBox();
+            this.lblShow = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(251, 61);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(75, 23);
+            this.btnCalc.TabIndex = 0;
+            this.btnCalc.Text = "计算";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(71, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "数字 A";
+            // 
+            // txtNuma
+            // 
+            this.txtNuma.Location = new System.Drawing.Point(112, 48);
+            this.txtNuma.Name = "txtNuma";
+            this.txtNuma.Size = new System.Drawing.Size(100, 20);
+            this.txtNuma.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(71, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "数字 B";
+            // 
+            // txtNumb
+            // 
+            this.txtNumb.Location = new System.Drawing.Point(112, 80);
+            this.txtNumb.Name = "txtNumb";
+            this.txtNumb.Size = new System.Drawing.Size(100, 20);
+            this.txtNumb.TabIndex = 2;
+            // 
+            // lblShow
+            // 
+            this.lblShow.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblShow.Location = new System.Drawing.Point(71, 113);
+            this.lblShow.Name = "lblShow";
+            this.lblShow.Size = new System.Drawing.Size(230, 44);
+            this.lblShow.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(394, 180);
+            this.Controls.Add(this.lblShow);
+            this.Controls.Add(this.txtNumb);
+            this.Controls.Add(this.txtNuma);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCalc);
+            this.Name = "Form1";
+            this.Text = "测试委托类型";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNuma;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNumb;
+        private System.Windows.Forms.Label lblShow;
+    }
+}
+
---
+++ Test8_1/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test8_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public delegate int Caculate(int x, int y);
+
+        public Caculate handler;
+
+        public int Product(int x, int y)
+        {
+            return x * y;
+        }
+
+        public int Average(int x, int y)
+        {
+            return (x + y) / 2;
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(txtNuma.Text);
+            int b = Convert.ToInt32(txtNumb.Text);
+
+            handler = new Caculate(Product);
+            lblShow.Text = "a 和 b 的乘积为：" + handler(a, b);
+
+            handler = new Caculate(Average);
+            lblShow.Text += "\na 和 b 的平均值为：" + handler(a, b);
+        }
+
+
+
+    }
+}
